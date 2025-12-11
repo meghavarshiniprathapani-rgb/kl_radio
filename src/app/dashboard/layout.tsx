44ba11f4b46727ae00a81b12c19851e491acb46f
Bhanuteja <nallamothubhanuteja110@gmail.com>
2025-12-11 11:36:22 +0000

    adjust the gap bw nav bar and the text

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
     <AuthProvider>
       <div className="flex min-h-screen w-full flex-col">
         <DashboardHeader />
-        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 pt-28">
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 pt-32">
           {children}
         </main>
       </div>
