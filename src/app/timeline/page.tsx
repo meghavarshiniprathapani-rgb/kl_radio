b176e7d0a3d1ec2ca27af01a32fac13fc3730c53
Bhanuteja <nallamothubhanuteja110@gmail.com>
2026-02-12 06:02:04 +0000

    https://ik.imagekit.io/tz33swtq7h/image.png
    use this for Initialization

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -138,7 +138,7 @@ const TimelinePage: React.FC = () => {
       title: 'Initialization of FunFiesta and had Successfully completed 4 Seasons till date',
       description: '',
       icon: <Globe className="text-white" />,
-      image: 'https://images.unsplash.com/photo-1590602848998-335b37b73e3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
+      image: 'https://ik.imagekit.io/tz33swtq7h/image.png',
       category: 'Content',
       color: 'amber',
     },
