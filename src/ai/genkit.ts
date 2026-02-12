da2b37e38306884da6e96b7c96d6bb3262e291cd
Bhanuteja <nallamothubhanuteja110@gmail.com>
2026-02-12 07:00:57 +0000

    make sure the ai feature in technical dashboard is working

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
+'use client';
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import next from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI(), next()],
   model: 'googleai/gemini-2.5-flash',
 });
